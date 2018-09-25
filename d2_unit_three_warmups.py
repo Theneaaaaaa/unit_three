c8b3c61054ca62d81863f07a4c2db064652f09f7
Thenea <thenea@iMac4.local>
2018-09-25 13:32:19 -0400

    function scope demonstration

---
+++ d2_unit_three_warmups.py
@@ -0,0 +1,10 @@
+def add_one(number):
+    number +=1
+    print("number in function is", number)  # 6
+
+def main():
+    number = 5
+    add_one(number)
+    print ("number in main is", number)  # 5
+
+main()
